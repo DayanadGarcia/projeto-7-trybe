6132a99680c4ead2b5a9f38466f995b29069d2ac
Dayana <dayanadiasgarcia@gmail.com>
2021-11-24 18:43:21 -0300

    Requisito 01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return species.filter((id) => ids.includes(id.id));
 }
 
 module.exports = getSpeciesByIds;
