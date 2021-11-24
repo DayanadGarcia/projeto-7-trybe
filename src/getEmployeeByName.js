3fc7854f85d380a3787e04998f1f0420f3479401
Dayana <dayanadiasgarcia@gmail.com>
2021-11-24 20:46:23 -0300

    Requisito 02

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+
+// Esta função é responsável pela busca das pessoas colaboradoras através do primeiro ou do último nome delas
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const funcionario = employees.filter(({
+    firstName,
+    lastName,
+  }) => firstName.includes(employeeName[0]) || lastName.includes(employeeName[1]));
+
+  return Object.assign({}, ...funcionario);
 }
 
+console.log(getEmployeeByName('Emery'));
 module.exports = getEmployeeByName;
