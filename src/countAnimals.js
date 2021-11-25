d78cc5b4964da14ac982a4ee7e565b0ddfb4d0a7
Dayana <dayanadiasgarcia@gmail.com>
2021-11-25 17:19:28 -0300

    Requisito 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const myObj = {};
+  if (!animal) {
+    species.forEach((elem) => {
+      myObj[elem.name] = elem.residents.length;// forma de add chave e valor no obj
+    });
+    return myObj;
+  }
+  const returnTotal = species.filter((el) => el.name.includes(animal.specie)); // retorna obj inteiro
+  if (animal.specie && animal.sex) {
+    const returnSex = returnTotal[0].residents.filter((elem) => elem.sex === animal.sex);
+    return returnSex.length;
+  }
+  if (animal.specie) {
+    const totalAnimais = returnTotal[0].residents.length;
+    return totalAnimais;
+  }
 }
-
 module.exports = countAnimals;
