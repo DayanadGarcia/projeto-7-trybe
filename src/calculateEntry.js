2787b218cf50e0a35da7bbc0d580f4bf45fbee99
Dayana <dayanadiasgarcia@gmail.com>
2021-11-26 18:40:46 -0300

    Requisito 06

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
+const calcPessoas = (accPessoas, currPessoa) => {
+  const acc = accPessoas;
+  if (currPessoa.age >= 18 && currPessoa.age < 50) {
+    acc.adult += 1;
+  } else if (currPessoa.age < 18) {
+    acc.child += 1;
+  } else if (currPessoa.age >= 50) {
+    acc.senior += 1;
+  }
+};
 function countEntrants(entrants) {
-  // seu código aqui
+  return entrants.reduce((accPessoas, currPessoa) => {
+    calcPessoas(accPessoas, currPessoa);
+    return accPessoas;
+  }, { child: 0, adult: 0, senior: 0 });
 }
-
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) { return 0; }
+  const myObj = countEntrants(entrants);
+  const crianca = (myObj.child) * prices.child;
+  const adulto = (myObj.adult) * prices.adult;
+  const idoso = (myObj.senior) * prices.senior;
+  return crianca + adulto + idoso;
 }
-
 module.exports = { calculateEntry, countEntrants };
