787f7b0c11555c3980e6d2014670b626acdb18ef
Dayana <dayanadiasgarcia@gmail.com>
2021-11-26 19:25:13 -0300

    Requisito 09

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
-const data = require('../data/zoo_data');
-
+const { employees } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+// A função busca por informações do animal mais velho da primeira espécie gerenciada pela pessoa colaboradora do parâmetro.
+// Passado o id de um funcionário, encontra a primeira espécie de animal gerenciado pelo funcionário, e retorna um array com nome, sexo e idade do animal mais velho dessa espécie.
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionario = employees.filter((elem) => elem.id.includes(id));// acessa pessoa
+  const animal = funcionario[0].responsibleFor[0];// acessa animal
+  const especie = species.find((elem) => elem.id.includes(animal));// acessa especie principal
+  const populaty = especie.residents;// acessa os animais residentes da especie
+  const maisVelho = populaty.sort((a, b) => b.age - a.age)[0];// ordena do maior pro menor e acessa o primeiro, ou seja, o mais velho
+  return Object.values(maisVelho);// retorna os valores do objeto
 }
-
 module.exports = getOldestFromFirstSpecies;
